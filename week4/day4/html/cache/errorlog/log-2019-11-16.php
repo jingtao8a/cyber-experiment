e9b5fa420ed497d5b56af997e50eb8f1118bf11a
yuxintao <1921056015@qq.com>
2023-07-01 14:53:07 +0800

    add week4

---
+++ week4/day4/html/cache/errorlog/log-2019-11-16.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-11-16 16:06:44 --> Unable to connect to the database
+ERROR - 2019-11-16 16:06:44 --> Severity: Error --> Call to a member function real_escape_string() on boolean /var/www/html/finecms/system/database/drivers/mysqli/mysqli_driver.php 391
---
+++ week4/day4/html/config/version.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+
+    'DR_UPDATE'		=> '2019.11.11',
+    'DR_VERSION'	=> '5.4.0',
+);
